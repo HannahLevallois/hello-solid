d17c221597c40c8536729ba2d76026332f154c92
Hannah Levallois <hannah.levallois@etu.minesparis.psl.eu>
2024-02-16 16:15:35 +0100

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 function App() {
   return (
+    <div>
     <p class="text-4xl text-green-700 text-center py-20">Hello les mines!</p>
-  );
+  
+    <p class="text-4xl text-green-700 text-center py-20">Hello la meuh!</p>
+  </div>);
 }
 
 export default App;
